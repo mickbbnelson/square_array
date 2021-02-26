3640d5231afe60ec7ee5d4c2c9e05c9b895cacd0
Mick Nelson <mickbbnelson@gmail.com>
2021-02-26 15:22:40 -0800

    done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,7 @@
 def square_array(array)
-  # your code here
+  new_array = []    #because this function will return the original value of the elements in an array, we need to creat a new value.
+  array.each do |number|
+    new_array << number ** 2    #the block is shoveling in elements into a new array
+  end              #This new array will bill filled with the squared elements because number ** 2
+  new_array       #** gives you the number to the power of 2 in this case
 end
